57d799e9a62cf9d1b4f5e7a066c1c2892c2fb218
JonWLS <jwon0100@student.monash.edu>
2023-03-21 19:11:38 +0800

    Add dropdown button to show all defined time slots

--- freeTimeFinderProject/finderApp/views.py
+++ freeTimeFinderProject/finderApp/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def algorithm():
+    return ['9:00 AM - 10:00 AM', '10:00 AM - 11:00 AM', '11:00 AM - 12:00 PM']
+
 def index(request):
-    return HttpResponse("Hello, world. Find your free time now!")
+    timeframes = algorithm()
+    context = {'values': timeframes}
+    return render(request, 'finder.html', context)
